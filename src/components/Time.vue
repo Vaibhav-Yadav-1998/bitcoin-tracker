d0d0128461c0e99176c26585495586bf37797015
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-08-10 20:08:53 +0530

    created time component

---
+++ src/components/Time.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="time">
+      Last Updated : {{time}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Time",
+  props: {
+    time: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+#time {
+  text-align: center;
+  font-weight: 600;
+}
+</style>
