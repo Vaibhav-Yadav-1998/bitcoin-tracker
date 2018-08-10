2cfc12f9a82d72d1a490206cea7e4fb32e846252
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-08-10 20:09:08 +0530

    added card and time component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
   <div id="app">
     <app-header />
+    <bitcoin-card :money="rate" />
+    <update-time :time="time" />
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader";
+import Card from "./components/Card";
+import Time from "./components/Time";
+import axios from "axios";
 
 export default {
   name: "app",
   components: {
-    "app-header": AppHeader
+    "app-header": AppHeader,
+    "bitcoin-card": Card,
+    "update-time": Time
+  },
+  data() {
+    return {
+      rate: "",
+      time: ""
+    };
+  },
+  methods: {
+    getPrice() {
+      const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+      axios
+        .get(url)
+        .then(res => {
+          this.rate = res.data.bpi.USD.rate;
+          this.time = res.data.time.updated;
+        })
+        .catch(err => {
+          console.log(err);
+          alert("sorry something went wrong, try again");
+        });
+    }
+  },
+  mounted() {
+    this.getPrice();
   }
 };
 </script>
